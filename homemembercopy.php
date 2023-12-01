0dbcb3c80cfc9e79551b551a807805931fa0680c
lu_adriankj <152279562+akvj1@users.noreply.github.com>
2023-12-01 02:58:42 -0800

    Add files via upload

---
+++ homemembercopy.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="stylehome.css">
+</head>
+
+ <body bgcolor="">
+    <h1 class="mem" align="center"><br>
+        MEMBERS
+    </h1>
+    <nav class="sidebar">
+        <header>
+         <div class="image">
+           <span class="img">
+             <img src="bgsrc/231-0998.jpg" alt="logo">
+           </span>
+           
+           <div class="text">
+              <span class="name">Adrian L. Javier</span>
+           </div>
+           
+         </div>
+        </header>
+     </nav>
+
+     <br>
+     <nav class="sidebar">
+        <header>
+         <div class="image">
+           <span class="img">
+             <img src="bgsrc/r.png" alt="logo">
+           </span>
+           
+           <div class="text">
+              <span class="name">Ricah Mendoza</span>
+           </div>
+           
+         </div>
+        </header>
+     </nav> 
+</body>
+</html>
